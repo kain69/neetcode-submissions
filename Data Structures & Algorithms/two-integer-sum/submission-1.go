047a0f393203e544c8378cdd7e2d3e4cd521e6bd
Vlad Karmazin <karmazin00@gmail.com>
2026-09-24 02:00:27 +0500

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+    existed := make(map[int]int)
+	for i, num := range nums {
+		if v, exists := existed[target - num]; exists {
+			return []int {v, i}
+		}
+		existed[num] = i
+	}
+	return []int{}
+}
